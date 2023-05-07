1ef11c027352c7b7786f988777b5cf0541d5a15a
Elmasloco <samuelleonardotarazona@hotmail.com>
2023-05-07 12:54:05 -0500

    actualizacion constructores

--- src/java/Modelo/Persona.java
+++ src/java/Modelo/Persona.java
@@ -22,7 +22,6 @@ public class Persona {
     private String genero;
     private int identificacion;
     private String tipoIdentificacion;
-    private String tipoAyuda;
 
     public Persona() {
     }
@@ -50,22 +49,6 @@ public class Persona {
         this.genero = resultSet.getString("genero");
         this.identificacion = resultSet.getInt("documento");
         this.tipoIdentificacion = resultSet.getString("tipoDoc");
-    }
-
-    public Persona(String nombre, String apellido, int identificacion, String tipoAyuda) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.identificacion = identificacion;
-        this.tipoAyuda = tipoAyuda;
-    }
-
-    public Persona(String nombre, String apellido, int edad, String genero, int identificacion, String tipoIdentificacion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.genero = genero;
-        this.identificacion = identificacion;
-        this.tipoIdentificacion = tipoIdentificacion;
     }
 
     public int getId() {
@@ -122,14 +105,6 @@ public class Persona {
 
     public void setTipoIdentificacion(String tipoIdentificacion) {
         this.tipoIdentificacion = tipoIdentificacion;
-    }
-
-    public String getTipoAyuda() {
-        return tipoAyuda;
-    }
-
-    public void setTipoAyuda(String tipoAyuda) {
-        this.tipoAyuda = tipoAyuda;
     }
 
     public HashMap toHashMap() {
